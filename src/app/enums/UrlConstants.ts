d7308863212446e7a637dbc2395d885ecaa070b0
jayad <jayadhasanofficial@gmail.com>
2025-08-31 14:21:03 +0600

    initial

---
+++ src/app/enums/UrlConstants.ts
@@ -0,0 +1,20 @@
+import { environment } from "../../environments/environments";
+export const UrlConstants = {
+
+  apiUrl: environment.apiUrl,
+  orgCOde:'00000196',
+  orgName:'Dhamrai Hardinge Govt High School & College(DHC)',
+  orgShortName:'DHC',
+  campusCOde:'00000113',
+  campusName:'Dhamrai',
+  userCOde:'DHC',
+
+
+  
+
+// Global Data List
+getResultSubjectList: "GlobalDataByOrg/GetSubjectList",
+
+
+
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  isMockEnabled: false,
+  LOCAL_STORAGE_LOGIN_SECRET_KEY:'U2FsdGVkX190/AT+n5N+K3Xo25gsEiw0Y1Qfgsgsgkc65XWWARx6Y9ZcrRWQIfnWyZurUi',
+
+  apiUrl: 'http://localhost:7097/api/',
+  devUrl: 'http://localhost:7097/',
+
+};
